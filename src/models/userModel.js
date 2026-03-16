187eb9eccf92eba16abbdb2f5aeeab6105a56b3a
Techforge25 <github@techforgeinnovations.com>
2026-03-17 01:00:37 +0500

    Add: Account verification fields to user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,11 +2,16 @@ const { Schema, model } = require("mongoose");
 
 // Schema
 const userSchema = new Schema({
+    // Basic info
     fullName: { type:String, trim:true, required:true },
     email: { type:String, trim:true, lowercase:true, required:true, unique:[true, "This email has already been taken"] },
     contact: { type:String, trim:true, required:true, unique:[true, "This contact number is already taken"] },
     streetName: { type:String, trim:true, required:true },
     address: { type:String, trim:true, required:true },
+
+    // Account verification otp
+    accountVerificationToken: { type:String, default:null },
+    accountVerificationTokenExpires: { type:Date, default:null },    
 }, { timestamps:true });
 
 // Model
