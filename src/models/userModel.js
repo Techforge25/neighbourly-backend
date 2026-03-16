1ba5dca9cc7e65cc07526818a5a346d471c0400f
Techforge25 <github@techforgeinnovations.com>
2026-03-17 01:45:05 +0500

    Keep fields optional initially on user schema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,16 +3,16 @@ const { Schema, model } = require("mongoose");
 // Schema
 const userSchema = new Schema({
     // Basic info
-    fullName: { type:String, trim:true, required:true },
+    fullName: { type:String, trim:true },
     email: { type:String, trim:true, lowercase:true, required:true, unique:[true, "This email has already been taken"] },
-    contact: { type:String, trim:true, required:true, unique:[true, "This contact number is already taken"] },
-    streetName: { type:String, trim:true, required:true },
-    address: { type:String, trim:true, required:true },
+    contact: { type:String, trim:true, unique:[true, "This contact number is already taken"] },
+    streetName: { type:String, trim:true },
+    address: { type:String, trim:true },
     status: { type:String, trim:true, enum:["pending", "approved"], default:"pending" },
 
     // Account verification otp
     accountVerificationToken: { type:String, default:null },
-    accountVerificationTokenExpires: { type:Date, default:null },    
+    accountVerificationTokenExpires: { type:Date, default:null },
 }, { timestamps:true });
 
 // Model
