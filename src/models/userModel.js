12a0ed1d0e487d24c2007bc331f5ac11727deb68
Techforge25 <github@techforgeinnovations.com>
2026-03-17 00:43:32 +0500

    Add email field to user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,6 +3,7 @@ const { Schema, model } = require("mongoose");
 // Schema
 const userSchema = new Schema({
     fullName: { type:String, trim:true, required:true },
+    email: { type:String, trim:true, lowercase:true, required:true, unique:[true, "This email has already been taken"] },
     contact: { type:String, trim:true, required:true, unique:[true, "This contact number is already taken"] },
     streetName: { type:String, trim:true, required:true },
     address: { type:String, trim:true, required:true },
