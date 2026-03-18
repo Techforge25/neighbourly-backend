1873d3c18f4c9c58dd0f21127a0cf9fd84b2be96
Techforge25 <github@techforgeinnovations.com>
2026-03-18 23:52:13 +0500

    Refactor middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,15 +1,29 @@
+const User = require("../models/userModel");
 const { getAccessToken, verifyAccessToken } = require("../utils/accessToken");
 const ApiError = require("../utils/ApiError");
 const asyncHandler = require("../utils/asyncHandler");
 
 // Authentication
-const authentication = asyncHandler((request, response, next) => {
+const authentication = asyncHandler(async (request, response, next) => {
     const accessToken = getAccessToken(request);
-    if(!accessToken) throw new ApiError(401, "Unauthorized!");
+    if(!accessToken) throw new ApiError(401, "Unauthorized! Please verify your identity via OTP");
 
     // Verify
     const user = verifyAccessToken(accessToken);
-    if(!user) throw new ApiError(401, "Invalid access token");
+    if(!user) throw new ApiError(401, "Invalid access token! Please verify your identity via OTP");
+
+    // Check session expiry
+    if(user.sessionExpires < Date.now())
+    {
+        // Expire session
+        const updateUser = await User.findByIdAndUpdate(
+            user._id,
+            { $set:{ isVerified:false } },
+            { new:true }
+        );
+        if(!updateUser) throw new ApiError(404, "User not found! Failed to expire user session");
+        throw new ApiError(403, "Your session has been expired! Please verify your identity via OTP")
+    }
 
     // Pass through
     request.user = user;
