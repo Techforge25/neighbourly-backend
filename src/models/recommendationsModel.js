b0d72f4834c694d0af1bc10c9609f4ae39748551
Techforge25 <github@techforgeinnovations.com>
2026-03-20 01:18:24 +0500

    Create model of recommendations

---
+++ src/models/recommendationsModel.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+// Schema
+const recommendationSchema = new Schema({
+    // Basic info
+    personName: { type:String, trim:true, required:true },
+    businessName: { type:String, trim:true, required:true },
+    contact: { type:String, trim:true, required:true },
+    serviceType: { type:String, trim:true, required:true },
+    location: { type:String, trim:true, required:true },
+
+    // Other info
+    website: { type:String, trim:true },
+    reasonsOfRecommendations:{ type:[String] }
+}, { timestamps:true });
+
+// Model
+const Recommendation = model("Recommendation", recommendationSchema);
+
+module.exports = Recommendation;
