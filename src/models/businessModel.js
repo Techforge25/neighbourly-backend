45165f548cebad26a402e3cf07bafb6feb0f4d4e
Techforge25 <github@techforgeinnovations.com>
2026-03-24 10:29:59 +0500

    Create business model

---
+++ src/models/businessModel.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+const aggregatePaginate = require("mongoose-aggregate-paginate-v2");
+
+// Schema
+const businessSchema = new Schema({
+    // Basic info
+    personName: { type:String, trim:true, required:true },
+    businessName: { type:String, trim:true, required:true, unique:[true, "This business name is already taken"] },
+    contact: { type:String, trim:true, required:true },
+    serviceType: { type:String, trim:true, required:true },
+    location: { type:String, trim:true, required:true },
+
+    // Other info
+    website: { type:String, trim:true },
+    reasonsOfRecommendation:{ type:[String] }
+});
+
+// Pagination plugin
+businessSchema.plugin(aggregatePaginate);
+
+// Compund indexing
+businessSchema.index({ personName: 1 });
+businessSchema.index({ businessName: 1 });
+
+// Model
+const User = model("User", businessSchema);
+
+module.exports = User;
