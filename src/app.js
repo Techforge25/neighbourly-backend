b741213132a29eca867f6fe3e0916cf364ed2957
Techforge25 <github@techforgeinnovations.com>
2026-03-24 15:34:43 +0500

    Register business route

--- src/app.js
+++ src/app.js
@@ -19,10 +19,12 @@ app.use(compression());
 // Import Routes
 const authRouter = require("./routes/authRoute");
 const recommendationRouter = require("./routes/recommendationRoute");
+const businessRouter = require("./routes/businessRoute");
 
 // Registered Routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/recommendation", recommendationRouter);
+app.use("/api/v1/business", businessRouter);
 
 // API status route
 app.get("/", (request, response) => response.send(`Neighbourly backend-server is up and running at port ${port}`));
