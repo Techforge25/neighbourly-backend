5806169ab455c1c7e5ba9be712d4046e315b736d
Techforge25 <github@techforgeinnovations.com>
2026-03-30 14:30:43 +0500

    Update: Send adresses key

--- src/controllers/statsController.js
+++ src/controllers/statsController.js
@@ -1,20 +1,25 @@
 const Business = require("../models/businessModel");
 const Recommendation = require("../models/recommendationsModel");
+const User = require("../models/userModel");
 const ApiResponse = require("../utils/ApiResponse");
 const asyncHandler = require("../utils/asyncHandler");
 
 // Fetch stats
 const fetchStats = asyncHandler(async (request, response) => {
     // Fetch
-    const [totalRecommendations, totalBusinesses] = await Promise.all([
+    const [totalRecommendations, totalBusinesses, address] = await Promise.all([
         Recommendation.countDocuments({}),
         Business.countDocuments({ recommendationCount:{ $gte:3 } }),
+
+        // Distinct addresses of users
+        User.distinct("address")
     ]);
 
     // Prepare payload
     const payload = {
         recommendations: totalRecommendations || 0,
-        businesses: totalBusinesses || 0
+        businesses: totalBusinesses || 0,
+        addresses: address.length || 0
     };
 
     // Response
