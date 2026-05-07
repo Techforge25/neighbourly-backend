fdaa46c2f07d13a8f7f02b597bbea9b09ca49dfa
Techforge25 <github@techforgeinnovations.com>
2026-05-07 16:42:23 +0500

    Add sponsorhip model

---
+++ src/models/sponsorModel.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+// Schema
+const sponsorSchema = new Schema({
+    // Basic info
+    logo: { type: String, trim: true, required: true },
+    personName: { type: String, trim: true, required: true },
+
+    // Business info
+    businessName: { type: String, trim: true, required: true },
+    serviceType: { type: String, trim: true, required: true },
+    contact: { type: String, trim: true, required: true },
+});
+
+// Model
+const Sponsor = model("Sponsor", sponsorSchema);
+
+module.exports = Sponsor;
