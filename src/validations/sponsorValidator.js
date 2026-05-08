017f43a06d163af836771bf710885a345a3b1834
Techforge25 <github@techforgeinnovations.com>
2026-05-08 16:07:48 +0500

    Add validator for sponsor feature

---
+++ src/validations/sponsorValidator.js
@@ -0,0 +1,18 @@
+const joi = require("joi");
+
+// Patterns
+const contactPattern = /^(?:\+?[1-9]\d{9,14}|0\d{9,14})$/;
+
+// Create sponsor validator
+const createSponsorValidator = joi.object({
+    logo: joi.string().trim().uri().required().label("Sponsor logo"),
+    personName: joi.string().trim().min(2).max(50).required().label("Person name"),
+    businessName: joi.string().trim().min(2).max(50).required().label("Business name"),
+    serviceType: joi.string().trim().min(2).max(50).required().label("Service type"),
+    contact: joi.string().trim().max(15).pattern(contactPattern).required().messages({
+        "string.pattern.base": "Contact number must be a valid international format (e.g., +923001234567)."
+    }).label("Contact"),
+    suburb: joi.string().trim().min(2).max(50).required().label("Suburb"),
+});
+
+module.exports = { createSponsorValidator };
