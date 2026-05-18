65325e3d41dc222b19dde78e72480e80fd0235f9
Techforge25 <github@techforgeinnovations.com>
2026-05-18 15:43:05 +0500

    Add admin panel origin

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ const frontendUrl = isProduction ? process.env.FRONTEND_URL : "http://localhost:
 
 // Cors options
 const corsOptions = {
-    origin:[process.env.ORIGIN, "http://localhost:3000", "https://www.suburbsays.com.au"],
+    origin:[process.env.ORIGIN, "http://localhost:3000", "https://www.suburbsays.com.au", "https://neighbourly-admin-panel-frontend.vercel.app"],
     credentials:true,
     methods:["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders:["Content-Type", "Authorization"]
