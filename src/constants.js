9ded1c01bd20cb516cb80b6a6a0aee9be5a1a69f
Techforge25 <github@techforgeinnovations.com>
2026-05-19 12:08:15 +0500

    Update cookie options

--- src/constants.js
+++ src/constants.js
@@ -18,7 +18,7 @@ const cookieOptions = {
     signed: true,
     maxAge: 1000 * 60 * 60 * 24 * 90,
     sameSite: isProduction ? "none" : "lax",
-    domain: ".beneighbourly.com.au",
+    // domain: ".beneighbourly.com.au",
     path: "/"
 };
 
