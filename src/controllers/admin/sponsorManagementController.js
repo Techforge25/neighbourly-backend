97e9b8d54fb66ca7984e96abf74d235803ee9d36
Techforge25 <github@techforgeinnovations.com>
2026-05-25 09:55:00 +0500

    Update fetch sponsor controller

--- src/controllers/admin/sponsorManagementController.js
+++ src/controllers/admin/sponsorManagementController.js
@@ -31,8 +31,7 @@ const fetchSponsors = asyncHandler(async (request, response) => {
 
     // Base filter
     const filter = {};
-    if(!suburb) return response.status(200).json(new ApiResponse(200, emptyList, "No sponsors found"));
-    if(suburb && suburb !== "all") filter.suburb = suburb;
+    if(suburb) filter.suburb = suburb;
 
     // Fetch
     const sponsors = await Sponsor.aggregatePaginate([
