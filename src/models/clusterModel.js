cd2b30367377474c701ca195241704ed394e6f07
Techforge25 <github@techforgeinnovations.com>
2026-06-09 16:27:04 +0500

    Model level validation

--- src/models/clusterModel.js
+++ src/models/clusterModel.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 // Schema
 const clusterSchema = new Schema({
-    name: { type:String, trim:true, required:true },
+    name: { type:String, trim:true, required:true, unique:[true, "Cluster with this name has laready exist"] },
     description: { type:String, trim:true, default: null }
 }, { timestamps: true });
 
