bb4f4b0006eb9701b1743cd683e111c138a54c90
Techforge25 <github@techforgeinnovations.com>
2026-06-09 16:57:30 +0500

    Add pagination plugin

--- src/models/clusterModel.js
+++ src/models/clusterModel.js
@@ -1,10 +1,14 @@
 const { Schema, model } = require("mongoose");
+const aggregatePaginate = require("mongoose-aggregate-paginate-v2");
 
 // Schema
 const clusterSchema = new Schema({
     name: { type:String, trim:true, required:true, unique:[true, "Cluster with this name has laready exist"] },
     description: { type:String, trim:true, default: null }
 }, { timestamps: true });
+
+// Pagination plugin
+clusterSchema.plugin(aggregatePaginate);
 
 // Model
 const Cluster = model("Cluster", clusterSchema);
