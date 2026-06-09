d6e706ce463898a579825b8925361d13bc364dcb
Techforge25 <github@techforgeinnovations.com>
2026-06-09 16:12:53 +0500

    Create validator for suburb creation

---
+++ src/validations/suburbValidator.js
@@ -0,0 +1,9 @@
+const joi = require("joi");
+
+// Create suburb validator
+const createSuburbValidator = joi.object({
+    name: joi.string().trim().min(2).max(50).required().label("Suburb"),
+    description: joi.string().trim().optional().allow(null, "").max(2000).required().label("Description")
+});
+
+module.exports = { createSuburbValidator };
