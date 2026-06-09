6d3c4cf656e0018f8e4194f517a2eaa27e3d8d0e
Techforge25 <github@techforgeinnovations.com>
2026-06-09 16:13:15 +0500

    Feat: create suburb

---
+++ src/controllers/admin/suburbManagementController.js
@@ -0,0 +1,35 @@
+const { isValidObjectId } = require("mongoose");
+const { emptyList } = require("../../constants");
+const ApiError = require("../../utils/ApiError");
+const ApiResponse = require("../../utils/ApiResponse");
+const asyncHandler = require("../../utils/asyncHandler");
+const validatePayload = require("../../utils/validatePayload");
+const { createSuburbValidator } = require("../../validations/suburbValidator");
+const Cluster = require("../../models/clusterModel");
+const Suburb = require("../../models/suburbModel");
+
+// Create suburb
+const createSuburb = asyncHandler(async (request, response) => {
+    const { clusterId } = request.params;
+    if(!isValidObjectId(clusterId)) throw new ApiError(400, "Invalid cluster ID");
+
+    // Get validated payload
+    const { name, description } = validatePayload(createSuburbValidator, request.body);
+
+    // Find cluster
+    const cluster = await Cluster.findById(clusterId).select("name").lean();
+    if(!cluster) throw new ApiError(404, "Cluster not found!");
+
+    // Get count
+    const suburbCount = await Suburb.countDocuments({ clusterId });
+    if(suburbCount >= 3) throw new ApiError(403, `${cluster.name} already have 3 suburbs`);
+
+    // Save to db
+    const suburb = await Suburb.create({ clusterId, name, description });
+    if(!suburb) throw new ApiError(500, "Failed to add suburb");
+
+    // Response
+    return response.status(201).json(new ApiResponse(201, null, "Suburb has been created"));
+});
+
+module.exports = { createSuburb };
---
+++ src/routes/admin/suburbManagementRoute.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const { adminAuthentication } = require("../../middlewares/adminAuth");
+const { createSuburb } = require("../../controllers/admin/suburbManagementController");
+
+// Router instance
+const suburbManagementRoute = Router();
+
+// Fetch Suburbs
+suburbManagementRoute.route("/:clusterId").post(adminAuthentication, createSuburb);
+
+module.exports = suburbManagementRoute;
