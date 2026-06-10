c2c7c3722d3eb500659e13cc847f90896a29df8c
Techforge25 <github@techforgeinnovations.com>
2026-06-10 09:39:51 +0500

    Add cluster ID in validator

--- src/validations/suburbValidator.js
+++ src/validations/suburbValidator.js
@@ -2,6 +2,7 @@ const joi = require("joi");
 
 // Create suburb validator
 const createSuburbValidator = joi.object({
+    clusterId: joi.string().trim().length(24).required().label("Cluster ID"),
     name: joi.string().trim().min(2).max(50).required().label("Suburb"),
     description: joi.string().trim().optional().allow(null, "").max(2000).required().label("Description")
 });
