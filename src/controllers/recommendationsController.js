6064702ee4a338f680412f96a9fb84af473a28d6
Techforge25 <github@techforgeinnovations.com>
2026-06-17 17:27:31 +0500

    Update api blueprint

--- src/controllers/recommendationsController.js
+++ src/controllers/recommendationsController.js
@@ -1,6 +1,7 @@
 const { emptyList } = require("../constants");
 const Business = require("../models/businessModel");
 const Recommendation = require("../models/recommendationsModel");
+const Suburb = require("../models/suburbModel");
 const User = require("../models/userModel");
 const ApiError = require("../utils/ApiError");
 const ApiResponse = require("../utils/ApiResponse");
@@ -229,5 +230,136 @@ const fetchRecommendations = asyncHandler(async (request, response) => {
     // Response
     return response.status(200).json(new ApiResponse(200, { recommendations, showFullList }, "Recommendations have been fetched"));
 });
+
+// Fetch recommendation (Update V2)
+// const fetchRecommendations = asyncHandler(async (request, response) => {
+//     let { page = 1, limit = 6, filter, location } = request.query;
+
+//     // Show list flag
+//     let showFullList = true;
+
+//     // // If not logged-in
+//     // if(!request.user)
+//     // {
+//     //     page = 1;
+//     //     limit = 3;
+//     //     showFullList = false;
+//     // }
+//     // else
+//     // {
+//     //     const userId = request.user._id;
+//     //     const user = await User.findOne({
+//     //         _id: userId,
+//     //         isProfileCompleted: true
+//     //     })
+//     //     .select("isProfileCompleted")
+//     //     .lean();
+
+//     //     if(!user)
+//     //     {
+//     //         page = 1;
+//     //         limit = 3;
+//     //         showFullList = false;
+//     //     }
+//     // }
+
+//     // Get all suburbs belonging to the selected suburb's cluster
+//     let clusterSuburbs = [];
+
+//     if(location) 
+//     {
+//         // Find suburb
+//         const suburb = await Suburb.findOne({ name: location }).select("clusterId").lean();
+//         if(!suburb) throw new ApiError(404, "No suburb found associated with this name");
+//         clusterSuburbs = await Suburb.find({ clusterId: suburb.clusterId }).distinct("name");
+//     }
+
+//     // Base filter
+//     const baseFilter = { status: "approved" };
+//     if(filter) baseFilter["business.serviceType"] = { $regex: filter, $options: "i" };
+    
+//     // Aggregation
+//     const recommendations = await Recommendation.aggregatePaginate([
+//         // Business lookup
+//         {
+//             $lookup: {
+//                 from: "businesses",
+//                 localField: "businessId",
+//                 foreignField: "_id",
+//                 as: "business"
+//             }
+//         },
+
+//         // User lookup
+//         {
+//             $lookup: {
+//                 from: "users",
+//                 localField: "userId",
+//                 foreignField: "_id",
+//                 as: "user"
+//             }
+//         },
+
+//         // Unwind
+//         { $unwind: { path: "$business", preserveNullAndEmptyArrays: true } },
+//         { $unwind: { path: "$user", preserveNullAndEmptyArrays: true } },
+
+//         // Match approved recommendations
+//         { $match: { ...baseFilter, "business.recommendationCount": { $gte: 3 } } },
+
+//         // Group by business
+//         {
+//             $group: {
+//                 _id: "$business._id",
+
+//                 businessId: { $first: "$business._id" },
+//                 personName: { $first: "$business.personName" },
+//                 businessName: { $first: "$business.businessName" },
+//                 businessContact: { $first: "$business.contact" },
+//                 serviceType: { $first: "$business.serviceType" },
+//                 addresses: { $addToSet: "$user.address" },
+
+//                 // Recommendation count
+//                 recommendationCount: {
+//                     $sum: {
+//                         $cond: [
+//                             location
+//                                 ? 
+//                                 { $in: ["$user.address", clusterSuburbs] }
+//                                 : true,
+//                             1,
+//                             0
+//                         ]
+//                     }
+//                 },
+
+//                 reasonsOfRecommendation: { $push: "$reasonsOfRecommendation" },
+//                 createdAt: { $first: "$business.createdAt" }
+//             }
+//         },
+
+//         // When location selected, only show businesses
+//         // having recommendations from that cluster
+//         ...(location
+//             ? [
+//                   {
+//                       $match: {
+//                           recommendationCount: { $gt: 0 }
+//                       }
+//                   }
+//               ]
+//             : []),
+
+//         // Sort
+//         { $sort: { recommendationCount: -1, createdAt: -1, businessId: 1 } },
+
+//         // Final projection
+//         { $project: { _id: 0, createdAt: 0 } }
+//     ], { page, limit });
+//     if(!recommendations.totalDocs) return response.status(200).json(new ApiResponse(200, { ...emptyList, showFullList },"No recommendations found"));
+    
+//     // Response
+//     return response.status(200).json(new ApiResponse(200, { recommendations, showFullList }, "Recommendations have been fetched"));
+// });
 
 module.exports = { createRecommendation, createRecommendationWithUserInfo, fetchRecommendations };
